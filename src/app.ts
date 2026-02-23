b043f5bde58aea45982c536588f9f36d68fe3e3d
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-02-23 21:19:57 +0600

    authentication continoue

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application } from "express"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from '../lib/auth';
 
 const app:Application = express();
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
