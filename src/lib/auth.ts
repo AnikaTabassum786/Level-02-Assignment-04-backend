426b7540ec5950e8c4ef54843995bc6ccd386aee
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-02-24 09:20:36 +0600

    Using better auth sign up and sign in done

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,6 +9,20 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    trustedOrigins:[process.env.APP_URL!],
+    user:{
+       additionalFields:{
+        role:{
+          type:"string",
+          defaultValue:"CUSTOMER",
+          required:false
+        },
+        phone:{
+            type:"string",
+            required:false
+           }
+       }
+    },
     emailAndPassword: { 
     enabled: true, 
   }, 
