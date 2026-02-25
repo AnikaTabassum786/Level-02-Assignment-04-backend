35eff99f82c505f3861f0f0b6405e93ebefea50f
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-02-25 12:28:23 +0600

    Category filter done

--- src/modules/medicine/medicine.service.ts
+++ src/modules/medicine/medicine.service.ts
@@ -2,56 +2,79 @@ import { prisma } from "../../lib/prisma"
 
 
 const createMedicine = async (data: {
-  name: string;
-  description: string;
-  price: string; // because Decimal
-  stock: number;
-  manufacturer: string;
-  imageURL?: string;
-  categoryId: string;
-  sellerId: string;
-})=>{
+    name: string;
+    description: string;
+    price: string; // because Decimal
+    stock: number;
+    manufacturer: string;
+    imageURL?: string;
+    categoryId: string;
+    sellerId: string;
+}) => {
     const result = await prisma.medicine.create({
-        data:{
+        data: {
             ...data,
         }
     })
     return result
 }
 
-const getAllMedicine=async(payload:{
-    search:string|undefined,
-    category:string|undefined,
-    manufacturer:string|undefined,
+const getAllMedicine = async (payload: {
+    search: string | undefined,
+    category: string | undefined,
+    manufacturer: string | undefined,
+
+
+}) => {
+    const whereCondition: any = {};
+     
+
+    if (payload.search) {
+        whereCondition.OR = [
+            {
+                name: {
+                    contains: payload.search,
+                    mode: "insensitive",
+                },
+            },
+            {
+                description: {
+                    contains: payload.search,
+                    mode: "insensitive",
+                },
+            },
+            {
+                manufacturer: {
+                    contains: payload.search,
+                    mode: "insensitive",
+                },
+            },
+        ];
+    }
+
    
 
-})=>{
+    if (payload.category) {
+        whereCondition.category = {
+            name: {
+                equals: payload.category,
+                mode: "insensitive",
+            },
+        };
+    }
 
- const result = await prisma.medicine.findMany({
-    where: {
-  OR: [
-    {
-      name: {
-        contains: payload.search as string,
-        mode: "insensitive",
-      },
-    },
-    {
-       manufacturer: {
-        contains: payload.search as string,
-        mode: "insensitive",
-      },
-    },
-  ],
-}
- });
- return result;
+    const result = await prisma.medicine.findMany({
+
+        where: whereCondition,
+        include: {
+            category: true,
+        },
+    });
+    return result;
 }
 
 
-export const medicineService={
+export const medicineService = {
     createMedicine,
     getAllMedicine
-    
-
 }
