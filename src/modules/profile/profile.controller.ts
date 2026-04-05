6e0d1829857060f033ccd49e8a597dcc6260f6a6
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-04-05 18:45:34 +0600

    Profile router modified

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -33,7 +33,7 @@ const updateProfileInfo = async (req: Request, res: Response) => {
         const result = await profileService.updateProfileInfo(user.id as string,req.body)
          res.status(200).json({
             success: true,
-            message: "Profile Fetched Successfully",
+            message: "Profile Updated Successfully",
             data: result
         })
     }
--- src/modules/profile/profile.route.ts
+++ src/modules/profile/profile.route.ts
@@ -5,7 +5,7 @@ import auth, { Role } from '../../middlewares/auth'
 const router = express.Router()
 
 
-router.get("/",auth(Role.ADMIN,Role.SELLER,Role.CUSTOMER),ProfileController.getProfileInfoByUser)
-router.patch("/",auth(Role.ADMIN,Role.SELLER,Role.CUSTOMER),ProfileController.updateProfileInfo)
+router.get("/",auth(Role.CUSTOMER),ProfileController.getProfileInfoByUser)
+router.patch("/",auth(Role.CUSTOMER),ProfileController.updateProfileInfo)
 
 export const profileRouter:Router = router
